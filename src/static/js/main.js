9500afbeac6823d561dd14f052b3b1fea9e36e26
DimitryVahr <48862110+DimitryVahr@users.noreply.github.com>
2019-07-27 18:47:02 +0400

    Add files via upload

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -1,10 +1,34 @@
 window.onload = function () {
-    let videoButton = document.querySelector('.video-button'); 
-    videoButton.onclick = function () {
-        $('.vidio-youtube').css('display','flex');
-        $('.video-button').addClass('hidden');
-        $('.video-description').addClass('hidden');
-    }  
+    // modal
+    var overlay = $('#overlay');
+    function openModal(modal) {
+        overlay.fadeIn(400, function () {
+            $(modal)
+                .css('display', 'flex')
+                .animate({ opacity: 1 }, 200);
+        });
+    };
+
+    function closeModal(modal) {
+        $(modal)
+            .animate({ opacity: 0 }, 200, function () {
+                $(this).hide();
+                overlay.fadeOut(400);
+            });
+    };
+
+    // esc press
+    $(document).keydown(function (e) {
+        if (e.keyCode === 27)
+            closeModal('.modal');
+    });
+    overlay.on("click", function () {
+        closeModal('#modal');
+    });
+    $('.video-button').on('click', function () {
+        openModal('#modal');
+    });
+
 }
 
 
